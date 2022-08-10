88fc7c85108dc4202936fb3c2fcc881111b91a61
sruly chaimowitz <srulychaimo@gmail.com>
2022-08-10 09:42:52 +0300

    back end work for allowing upload files using multer

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import path from "path";
 import cors from "cors";
 import dotenv from "dotenv";
 import productsRouter from "./routes/productRoutes.js";
 import usersRouter from "./routes/userRoutes.js";
+import uploadRouter from "./routes/uploadRoutes";
 import orderRouter from "./routes/orderRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
@@ -17,10 +19,14 @@ app.use(cors());
 app.use("/api/products", productsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/orders", orderRouter);
+app.use("/api/upload", uploadRouter);
 
 app.get("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID);
 });
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(notFound);
 app.use(errorHandler);
