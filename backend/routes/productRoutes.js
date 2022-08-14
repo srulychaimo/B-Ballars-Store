a6ad362345de0a21970c166c900a40fefe14cdff
sruly chaimowitz <srulychaimo@gmail.com>
2022-08-14 15:06:45 +0300

    adding backend endpoint to add a review

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 import {
   createProduct,
+  createProductReview,
   deleteProduct,
   getProductById,
   getProducts,
@@ -11,6 +12,7 @@ import {
 import { admin, auth } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts).post(auth, admin, createProduct);
+router.route("/:id/reviews").post(auth, createProductReview);
 
 router
   .route("/:id")
