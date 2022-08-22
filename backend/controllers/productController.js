db9e632f64185f639f4bde71962c5bbca9a8a0d0
sruly chaimowitz <srulychaimo@gmail.com>
2022-08-22 11:47:36 +0300

    edditing the title name & uplaoad folder

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,7 +6,7 @@ import Product from "../models/productModel.js";
 // @access  public
 
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 10;
+  const pageSize = 12;
   const page = Number(req.query.pageNumber) || 1;
 
   const keyword = req.query.keyword
